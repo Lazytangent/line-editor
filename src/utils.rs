e3c4953543adf0613892d236ae692f1e5eeff385
Lazytangent <peter.tn.mai@gmail.com>
2022-10-10 14:05:13 -0500

    Create new file if one doesn't exist
    Adds an empty line to the file if the file doesn't have any lines

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,23 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader},
+    io::{self, BufRead, BufReader, Write},
 };
 
 pub fn open_file(path: &str) -> io::Result<Vec<String>> {
-    let f = File::open(path).unwrap();
+    let f = match File::open(path) {
+        Ok(f) => f,
+        Err(_) => {
+            let mut file = File::create(path).unwrap();
+            file.write_all(b"\n").unwrap();
+            file
+        }
+    };
     let reader = BufReader::new(f);
 
-    let contents: Vec<String> = reader.lines().map(|res| res.unwrap()).collect();
+    let mut contents: Vec<String> = reader.lines().map(|res| res.unwrap()).collect();
+    if contents.len() == 0 {
+        contents.push(String::new());
+    }
 
     Ok(contents)
 }
