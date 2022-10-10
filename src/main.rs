b652b51c907df498a5df8c81d0279a52d3f40b23
Lazytangent <peter.tn.mai@gmail.com>
2022-10-09 19:35:50 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use rustea::{
+    crossterm::event::{KeyCode, KeyEvent, KeyModifiers}
+    App,
+    Command,
+    Message,
+};
+
+struct Model {
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
